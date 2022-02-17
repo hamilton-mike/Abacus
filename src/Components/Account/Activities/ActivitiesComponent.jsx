762e09e72c6723b6d128b3c704b6934e8e5f2cdc
Michael Hamilton <hmisaiah30@gmail.com>
2022-02-16 22:49:37 -0700

    style account page needs graph chart

---
+++ src/Components/Account/Activities/ActivitiesComponent.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ActivitiesComponent = () => {
+  return (
+      <>
+        <section id="recent">
+            <article>
+              <div>
+                  <h2>Activities</h2>
+                  <select name="" id="">
+                      <option value="">January</option>
+                      <option value="">February</option>
+                      <option value="">March</option>
+                      <option value="">April</option>
+                      <option value="">May</option>
+                      <option value="">June</option>
+                      <option value="">July</option>
+                      <option value="">August</option>
+                      <option value="">September</option>
+                      <option value="">October</option>
+                      <option value="">November</option>
+                      <option value="">December</option>
+                  </select>
+              </div>
+            </article>
+        </section>
+      </>
+  )
+}
+
+export default ActivitiesComponent
