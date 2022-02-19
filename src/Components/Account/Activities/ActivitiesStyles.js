2d36bfb5ff87604d7f963425806151a9ffdd2c6d
Michael Hamilton <hmisaiah30@gmail.com>
2022-02-19 01:30:45 -0700

    made two folders one for back other for front. reason for recent deleted files on front-end side

--- src/Components/Account/Activities/ActivitiesStyles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const Section = styled.section`
-    margin-left: 4em;
-`;
--- src/globalStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Flex = styled.div`
-    display: flex;
-`
-
-export const RedBorder = styled.div`
-    border: 2px solid red;
-`
