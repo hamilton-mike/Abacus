8e4ae1f433bcbce492e6ab28c13b9e928a2a39bd
Michael Hamilton <hmisaiah30@gmail.com>
2022-02-19 00:48:51 -0700

    set up backend folder | app listening on port 9000

---
+++ back/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const DB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/'
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.once('open', () => console.log('connected to mongoose...'))
---
+++ back/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+require('./db/mongoose');
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 9000;
+
+
+const corsOptions = {
+    "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "allowedHeaders": ["Content-Type"],
+    origin: (origin, cb) => {
+        if (urlArray.indexOf(origin) != -1 || !origin) {
+            cb(null, true)
+        } else {
+            cb(new Error(`Origin not allowed by CORS!!!: ${origin}`))
+        }
+    }
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
