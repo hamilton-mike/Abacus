c9421927db6b5fc38c6fa15327f5d1065ac7ef7f
Michael Hamilton <hmisaiah30@gmail.com>
2022-02-19 04:10:01 -0700

    hash passwords

--- back/controllers/user-restful.js
+++ back/controllers/user-restful.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const User = require('../models/UserSchema');
 
-router.get('/', (req, res) => {
-    res.send('hello')
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+router.post('/signup', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        const users = await User.find();
+        const duplicates = users.filter(obj => obj.username === newUser.username);
+        const hash = await bcrypt.hash(newUser.password, 10);
+        newUser.password = hash;
+        (duplicates.length > 1) ? res.redirect('https://abacus-ga.netlify.app/') : res.status(200).json(newUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const editUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json(editUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const destroyUser = await User.findByIdAndRemove(req.params.id)
+        res.status(200).json(destroyUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 module.exports = router;
--- back/models/UserSchema.js
+++ back/models/UserSchema.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const str = { type: String };
-const num = { type: Number }
+const num = { type: Number };
 
 const userSchema = mongoose.Schema({
-    name: str,
-    email: str,
-    hash: str,
+    name: { type: String, require: true },
+    username: str,
+    password: str,
     income: num,
     debt: num,
     age: num
