477efe2c0aa55ea7f0b1277b2a4906f384fb1b05
Michael Hamilton <hmisaiah30@gmail.com>
2022-02-20 01:23:25 -0700

    connect front to back | if user email exist dont make duplicates

--- front/src/Components/SignUpForm/SignUp.jsx
+++ front/src/Components/SignUpForm/SignUp.jsx
@@ -1,41 +1,63 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
 import { InputStyle, LabelStyle, FormDiv, ConfirmStyle, EmailStyle } from "./SignUpFormStyles";
+import axios from 'axios';
 
 const SignUp = () => {
-    const navigate = useNavigate();
+    const init = { email: '', pw: '', pw2: '' };
+    // const navigate = useNavigate();
+    // navigate('/home')
+    const [userInput, setUserInput] = useState(init)
 
-    const homePage = () => {
-        navigate('/home')
+    const handleChange = e => {
+        setUserInput({ ...userInput, [e.target.name]: e.target.value })
     }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        makeUser(userInput);
+    }
+
+    const makeUser = async obj => {
+        try {
+            const formData = await axios.post('http://localhost:9000/user/signup', {
+                username: obj.email,
+                password: obj.pw2
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // name:
+    // username:
+    // password:
+    // income:
+    // debt:
+    // age:
 
     return (
         <>
-            <div onSubmit={homePage}>
-                <FormDiv>
-                    <LabelStyle htmlFor="uname">Username</LabelStyle>
-                    <InputStyle type="text" required name='uname'/>
-                </FormDiv>
-
+            <form onSubmit={handleSubmit}>
                 <FormDiv>
                     <EmailStyle htmlFor="pw">Email</EmailStyle>
-                    <InputStyle type="email" required name='email'/>
+                    <InputStyle type="email" required name='email' onChange={handleChange} />
                 </FormDiv>
 
                 <FormDiv>
                     <LabelStyle htmlFor="pw">Password</LabelStyle>
-                    <InputStyle type="password" required name='pw' />
+                    <InputStyle type="password" required name='pw'  onChange={handleChange} />
                 </FormDiv>
 
                 <FormDiv>
                     <ConfirmStyle htmlFor="pw2">Confirm</ConfirmStyle>
-                    <InputStyle type="password" required name='pw2'/>
+                    <InputStyle type="password" required name='pw2' onChange={handleChange} />
                 </FormDiv>
 
                 <FormDiv>
                     <InputStyle type="submit" value="Register" />
                 </FormDiv>
-            </div>
+            </form>
         </>
     )
 }
