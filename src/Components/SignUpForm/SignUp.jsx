610ec2ae8086df1544bbeb41013faabf8bf06ef6
cokorie <chijioke.okorie@gmail.com>
2022-02-21 22:37:36 -0600

    Update Signup

--- src/Components/SignUpForm/SignUp.jsx
+++ src/Components/SignUpForm/SignUp.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
-import { InputStyle, BtnStyle, BG, Dblock , LabelStyle} from "./SignUpFormStyles.js";
-
+import { useNavigate } from 'react-router-dom'
+import { InputStyle, BtnStyle, BG, Dblock , LabelStyle} from "./SignUpFormStyles";
 
 const SignUp = () => {
-  return (
-    <>
-        <Dblock method='POST'>
-            <div>
-                <LabelStyle htmlFor="uname">Username</LabelStyle>
-                <InputStyle type="text" placeholder='Username' required name='uname'/>
-            </div>
+    const navigate = useNavigate();
 
-            <div>
-                <LabelStyle htmlFor="pw">Password</LabelStyle>
-                <InputStyle type="password" placeholder='Password' required name='pw'/>
-            </div>
+    const homePage = () => {
+        navigate('/home')
+    }
 
-            <div>
-                <LabelStyle htmlFor="pw2">Confirm</LabelStyle>
-                <InputStyle type="password" placeholder='Confirm Password' required name='pw2'/>
-                <InputStyle type="submit" value="Register" />
-            </div>
-        </Dblock>
-    </>
-  )
+    return (
+        <>
+            <Dblock onSubmit={homePage}>
+                <div>
+                    <LabelStyle htmlFor="uname">Username</LabelStyle>
+                    <InputStyle type="text" placeholder='Username' required name='uname'/>
+                </div>
+
+                <div>
+                    <LabelStyle htmlFor="pw">Password</LabelStyle>
+                    <InputStyle type="password" placeholder='Password' required name='pw'/>
+                </div>
+
+                <div>
+                    <LabelStyle htmlFor="pw2">Confirm</LabelStyle>
+                    <InputStyle type="password" placeholder='Confirm Password' required name='pw2'/>
+                    <InputStyle type="submit" value="Register" />
+                </div>
+            </Dblock>
+        </>
+    )
 }
 
 export default SignUp
